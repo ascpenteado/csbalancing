16ba97b076216a585afa6cb89748cdea6912e6a9
Arthur Penteado <ascpenteado@gmail.com>
2020-11-13 20:40:35 -0300

    last update

--- index.js
+++ index.js
@@ -14,44 +14,99 @@ class CustomerSuccessBalancing {
         return b.account - a.account;
     }
 
-    execute() {
-        const sortedCustomers = this.customers.sort(this.scoreAsc);
+    csSearch(array, target) {
+        let startIndex = 0;
+        let endIndex = array.length - 1;
 
-        const activeCS = this.cs.filter((x) => !this.cs_away.includes(x.id)).sort(this.scoreAsc);
-
-        const qualifiedCS = activeCS.filter((x) => x.score >= sortedCustomers[0].score);
-
-        if (qualifiedCS.length < 1) {
-            return 0;
-        }
-
-        // For each qualified CS, check Customer Level against CS Score
-        for (let i = 0; i < qualifiedCS.length; i++) {
-            let totalAccounts = 0;
-            for (let k = 0; k < sortedCustomers.length; k++) {
-                if (sortedCustomers[k].score <= qualifiedCS[i].score) {
-                    totalAccounts++;
-                    sortedCustomers.splice(sortedCustomers[k], 1);
-                    k--;
-                }
+        while (startIndex <= endIndex) {
+            if (target <= array[startIndex].score) {
+                return array[startIndex].id;
             }
-            qualifiedCS[i].accounts = totalAccounts;
-            // console.log(qualifiedCS[i]);
-        }
 
-        // Check for a tie
-        for (let i = 0; i < qualifiedCS.length; i++) {
+            let middleIndex = Math.floor((startIndex + endIndex) / 2);
+            let nextIndex = middleIndex + 1;
+            let prevIndex = middleIndex - 1;
+
+            if (nextIndex > endIndex) {
+                nextIndex = endIndex;
+            }
+
+            if (prevIndex < startIndex) {
+                prevIndex = startIndex;
+            }
+
+            if (target === array[middleIndex].score) {
+                return array[middleIndex].id;
+            }
+
+            if (target > array[middleIndex].score && target <= array[nextIndex].score) {
+                return array[nextIndex].id;
+            }
+
+            if (target < array[middleIndex].score && target >= array[prevIndex].score) {
+                return array[middleIndex].id;
+            }
+
+            if (target > array[middleIndex].score) {
+                startIndex = middleIndex + 1;
+            }
+
+            if (target < array[middleIndex].score) {
+                endIndex = middleIndex - 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    checkForTie(array) {
+        for (let i = 0; i < array.length; i++) {
             if (i > 0) {
-                if (qualifiedCS[i].accounts !== qualifiedCS[i - 1].accounts) {
+                if (array[i].accounts !== array[i - 1].accounts) {
                     break;
                 } else {
                     return 0;
                 }
             }
         }
+    }
 
-        // Return CSM with the highest accounts number
-        return qualifiedCS.sort(this.accountDesc)[0].id;
+    getCSM(array) {
+        const tie = this.checkForTie(array);
+        if (tie === 0) {
+            return 0;
+        }
+        return array.sort(this.accountDesc)[0].id;
+    }
+
+    execute() {
+        const sortedCustomers = this.customers.sort(this.scoreAsc);
+
+        const activeCS = this.cs.filter((x) => !this.cs_away.includes(x.id)).sort(this.scoreAsc);
+
+        const qualifiedCS = activeCS
+            .map((aCS) => {
+                let newAcs = Object.assign({}, aCS);
+                newAcs.accounts = 0;
+                return newAcs;
+            })
+            .filter((x) => x.score >= sortedCustomers[0].score);
+
+        if (qualifiedCS.length < 1) {
+            return 0;
+        }
+
+        // For each customer associate a CSM
+        sortedCustomers.forEach((customer) => {
+            const csm = this.csSearch(qualifiedCS, customer.score);
+            if (csm === 0) {
+                return 0;
+            }
+            qualifiedCS.find((qCS) => qCS.id === csm).accounts++;
+        });
+
+        // Return CSM
+        return this.getCSM(qualifiedCS);
     }
 }
 
--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const assert = require("chai").assert;
-const CustomerSuccessBalancing = require("../v2.js");
+const CustomerSuccessBalancing = require("../index.js");
 
 // Array to Map Helper
-function array_to_map(arr) {
+function arrayToMap(arr) {
     const out = [];
     arr.forEach((el, ind) => {
         out.push({ id: ind + 1, score: el });
@@ -35,9 +35,9 @@ describe("Customer Success Balancing", function () {
     });
 
     it("test_scenario_two should return 0", function () {
-        const css = array_to_map([11, 21, 31, 3, 4, 5]);
+        const css = arrayToMap([11, 21, 31, 3, 4, 5]);
 
-        const customers = array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+        const customers = arrayToMap([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
 
         const balancer = new CustomerSuccessBalancing(css, customers, []);
         const result = balancer.execute();
@@ -47,14 +47,14 @@ describe("Customer Success Balancing", function () {
 
     it("test_scenario_three should return 999", function () {
         this.timeout(1000);
-        const customer_success = new Array(1000, 0);
-        customer_success.fill(0);
-        customer_success[998] = 100;
+        const customerSuccess = new Array(1000, 0);
+        customerSuccess.fill(0);
+        customerSuccess[998] = 100;
 
-        const customers = new Array(100000);
+        const customers = new Array(10000);
         customers.fill(10);
 
-        const balancer = new CustomerSuccessBalancing(array_to_map(customer_success), array_to_map(customers), [1000]);
+        const balancer = new CustomerSuccessBalancing(arrayToMap(customerSuccess), arrayToMap(customers), [1000]);
         const result = balancer.execute();
 
         assert.equal(result, 999);
@@ -62,8 +62,8 @@ describe("Customer Success Balancing", function () {
 
     it("test_scenario_four should return 0", function () {
         const balancer = new CustomerSuccessBalancing(
-            array_to_map([1, 2, 3, 4, 5, 6]),
-            array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+            arrayToMap([1, 2, 3, 4, 5, 6]),
+            arrayToMap([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
             []
         );
         const result = balancer.execute();
@@ -73,8 +73,8 @@ describe("Customer Success Balancing", function () {
 
     it("test_scenario_five should return 1", function () {
         const balancer = new CustomerSuccessBalancing(
-            array_to_map([100, 2, 3, 3, 4, 5]),
-            array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+            arrayToMap([100, 2, 3, 3, 4, 5]),
+            arrayToMap([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
             []
         );
         const result = balancer.execute();
@@ -83,8 +83,8 @@ describe("Customer Success Balancing", function () {
 
     it("test_scenario_six should return 0", function () {
         const balancer = new CustomerSuccessBalancing(
-            array_to_map([100, 99, 88, 3, 4, 5]),
-            array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+            arrayToMap([100, 99, 88, 3, 4, 5]),
+            arrayToMap([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
             [1, 3, 2]
         );
         const result = balancer.execute();
@@ -93,8 +93,8 @@ describe("Customer Success Balancing", function () {
 
     it("test_scenario_seven should return 3", function () {
         const balancer = new CustomerSuccessBalancing(
-            array_to_map([100, 99, 88, 3, 4, 5]),
-            array_to_map([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+            arrayToMap([100, 99, 88, 3, 4, 5]),
+            arrayToMap([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
             [4, 5, 6]
         );
         const result = balancer.execute();
